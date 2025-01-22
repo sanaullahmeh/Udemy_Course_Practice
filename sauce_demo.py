6b2bfa4e212ec012b6b8444cd44399268e373de9
sanaullahmeh <sanaullahmehrani4@gmail.com>
2025-01-22 14:21:30 +0500

    Sauce Demo Automation

--- sauce_demo.py
+++ sauce_demo.py
@@ -3,9 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+
 BaseURL = 'https://www.saucedemo.com/'
 username = 'standard_user'
 password = 'secret_sauce'
+first_name = "John"
+last_name = "Doe"
+postal_code = 5474
 
 class SauceDemo:
     def login(self,driver):
@@ -35,16 +39,40 @@ class SauceDemo:
             EC.element_to_be_clickable((By.CSS_SELECTOR,".shopping_cart_link"))
         )
         cart_icon.click()
-        time.sleep(2)
         checkout_button = WebDriverWait(driver,10).until(
             EC.element_to_be_clickable((By.ID,"checkout"))
         )
         driver.execute_script("arguments[0].scrollIntoView()" , checkout_button)
         checkout_button.click()
-        time.sleep(2)
-        
+        firstname_field = WebDriverWait(driver,10).until(
+            EC.element_to_be_clickable((By.ID,"first-name"))
+        )
+        firstname_field.send_keys(first_name)
 
+        lastname_field = WebDriverWait(driver,10).until(
+            EC.element_to_be_clickable((By.ID,"last-name"))
+        )
+        lastname_field.send_keys(last_name)
 
+        postalcode_field = WebDriverWait(driver,10).until(
+            EC.element_to_be_clickable((By.ID,"postal-code"))
+        )
+        postalcode_field.send_keys(postal_code)
+        continue_button = driver.find_element(By.XPATH,"//input[@id='continue']")
+        continue_button.click()
+
+        item_total = driver.find_element(By.CSS_SELECTOR,".summary_subtotal_label").text
+        assert item_total == 'Item total: $129.94', f"Expected Item total price is not as expected. Found: {item_total}"
+
+        tax_percentage = driver.find_element(By.CSS_SELECTOR,".summary_tax_label").text
+        assert '$10.40' in tax_percentage , f"Expected tax to be 10.40 but found {tax_percentage}"
+
+        finish_button = driver.find_element(By.CSS_SELECTOR,"#finish")
+        finish_button.click()
+
+        success_message = driver.find_element(By.CSS_SELECTOR,".complete-header").text
+        assert "Thank you" in success_message , f"checkout process failed"
+        print(f"Whole Flow Automated of Sauce Demo - {success_message}")
 
 driver = webdriver.Chrome()
 driver.maximize_window()
